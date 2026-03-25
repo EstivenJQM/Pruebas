62e448018af4826dae38c5d5b5709dff1eb84e22
EstivenJQM <juan_quiroz82202@elpoli.edu.co>
2026-03-25 12:13:07 -0500

    fix (ya se pueden agregar componentes, lineas, actividades)

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,9 @@ use App\Http\Controllers\ComponenteController;
 use App\Http\Controllers\LineaController;
 use App\Http\Controllers\ActividadController;
 
-// Ruta principal (puedes dejarla o cambiarla)
 Route::get('/', function () {
     return view('welcome');
 });
-
-// NUEVA RUTA
-Route::get('/eventos', [EventoController::class, 'index']);
-
-// RUTAS PARA FILTROS
-Route::get('/componentes/{id}', [EventoController::class, 'getComponentes']);
-Route::get('/lineas/{id}', [EventoController::class, 'getLineas']);
-Route::get('/actividades/{id}', [EventoController::class, 'getActividades']);
 
 // CRUD
 Route::resource('areas',       AreaController::class);
@@ -26,8 +17,17 @@ Route::resource('componentes', ComponenteController::class);
 Route::resource('lineas',      LineaController::class);
 Route::resource('actividades', ActividadController::class);
 
-// Dropdowns dinámicos (los que ya tenías)
+// Dropdowns dinámicos
 Route::get('/eventos', [EventoController::class, 'index']);
 Route::get('/api/componentes/{id}', [EventoController::class, 'getComponentes']);
 Route::get('/api/lineas/{id}',      [EventoController::class, 'getLineas']);
 Route::get('/api/actividades/{id}', [EventoController::class, 'getActividades']);
+
+/*
+// NUEVA RUTA
+Route::get('/eventos', [EventoController::class, 'index']);
+
+// RUTAS PARA FILTROS
+Route::get('/componentes/{id}', [EventoController::class, 'getComponentes']);
+Route::get('/lineas/{id}', [EventoController::class, 'getLineas']);
+Route::get('/actividades/{id}', [EventoController::class, 'getActividades']);*/
